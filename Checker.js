a0527da82d6511d9d65873daaf64dcbde0f13e66
LennardKu <104419069+LennardKu@users.noreply.github.com>
2022-12-06 14:21:36 +0100

    v2.1 commit

--- Checker.js
+++ Checker.js
@@ -3,11 +3,14 @@
 const formChecker = {
   version: '2.1',
   repoUrl: 'https://github.com/LennardKu/Password-Checker',
+  tooltipTitle: 'Password must contain',
+  tooltipNames: {'capitalLetters':'Capital letters','lowerCaseLetters': 'Lowercase letters', 'numbers': 'Numbers','specialCharacters': 'Special characters' }, 
   miniumPasswordLength: 4,
   capitalLetters: 2,
   lowerCaseLetters: 2,
   numbers: 2,
   specialCharacters: 2,
+  colors:{'weak':'red','medium':'yellow','strong':'green'},
 
   init: async function () {
 
@@ -19,7 +22,25 @@ const formChecker = {
       await new formChecker.config(checkableForms[index],options);
     }
 
+    formChecker.colorConfig();
     this.log('Init complete',`Setup`);
+  },
+
+  colorConfig: function (colors) {    
+
+    let userColors = document.createElement('style'),
+    userAddedColors = ':root{',
+    setColors = (colors || this.colors);
+
+    for (let color in setColors) {
+      if (Object.hasOwnProperty.call(setColors, color)) {
+        userAddedColors = `${userAddedColors} --${color}: ${setColors[color]};`;
+      }
+    }
+    userAddedColors = `${userAddedColors} }`;
+    userColors.innerHTML = `${userAddedColors}`;
+    document.querySelectorAll('head')[0].appendChild(userColors);
+  
   },
 
   config: function (element,options) {
@@ -54,12 +75,11 @@ const formChecker = {
     }
 
     element.setAttribute('data-form-checker', minimum.length);
-    formChecker.passwordResponse(element, 'progressbar');
+    formChecker.passwordResponse(element, 'toolprogress');
 
-    // ! Keys up 
-    element.addEventListener('keyup', function() {
-
-      if (formChecker.checkOption('email-checker', options)) {
+    if (formChecker.checkOption('email-checker', options)) {
+      let emailInput = element.querySelectorAll('[type="email"]')[0];
+      emailInput.addEventListener('keyup', function() {
         if (!formChecker.emailValidator(element)) {
           if (!doneWith.includes('email-checker')) {
             doneWith.push('email-checker');
@@ -69,23 +89,35 @@ const formChecker = {
             doneWith.splice(doneWith.indexOf('email-checker'), 1);
           }
         }
-      }
+      });
+    }
+
+    if (formChecker.checkOption('password-repeat', options)) {
+
+      let passwordInput = element.querySelectorAll('[type="password"]');
       
-      if (formChecker.checkOption('password-repeat', options)) {
-        if (formChecker.checkRepeatedPassword(element)){
-          if (!doneWith.includes('password-repeat')) {
-            doneWith.push('password-repeat');
+        for (let index = 0; index < passwordInput.length; index++) {     
+          
+           // Check for password Checker 
+           if(passwordInput.length == (index + 1)){
+            passwordInput[index].setAttribute('disabled',true);
           }
-        } else {
-          if (doneWith.includes('password-repeat')) {
-            doneWith.splice(doneWith.indexOf('password-repeat'), 1);
+
+          passwordInput[index].addEventListener('keyup', function() {
+            if (formChecker.checkRepeatedPassword(element)){
+              if (!doneWith.includes('password-repeat')) {
+                doneWith.push('password-repeat');
+              }
+            } else {
+              if (doneWith.includes('password-repeat')) {
+                doneWith.splice(doneWith.indexOf('password-repeat'), 1);
+              }
           }
-        }
-      }
-
-      formChecker.log(minimum,'minimum');
-      formChecker.log(doneWith,'doneWith')
-
+      });
+     }
+    }
+    
+    element.addEventListener('keyup', function() {
       if(formChecker.checkOption('button', options)){
         let button = element.querySelectorAll('[type="submit"]');
         if (minimum.length == doneWith.length && button.length) {
@@ -94,7 +126,6 @@ const formChecker = {
           button[0].setAttribute('disabled', true);
         }
       }
-
     });
     
   },
@@ -139,13 +170,13 @@ const formChecker = {
     }
     
     let emailInput =  element.querySelectorAll('input[type="email"]')[0];
-    console.log(formChecker.emailChecker(emailInput.value));
+
     if(!formChecker.emailChecker(emailInput.value)){
-      formChecker.respone(emailInput,'weak');
+      formChecker.response(emailInput,'weak');
       return true;
     } 
 
-    formChecker.respone(emailInput,'strong');
+    formChecker.response(emailInput,'strong');
     return false;
   },
 
@@ -153,7 +184,7 @@ const formChecker = {
     return email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
   },
 
-  respone: function (element,status) {
+  response: function (element,status) {
     element.classList.remove('weak', 'medium', 'strong');
     element.classList.add(status);
   },  
@@ -178,7 +209,7 @@ const formChecker = {
       return false;
     }
 
-    let ForbiddenSequences = ["123456","password","12345678","1234","pussy","12345","dragon","qwerty","696969","mustang","letmein","baseball","master","michael","football","shadow","monkey","abc123","pass","6969","jordan","harley","ranger","iwantu","jennifer","hunter","2000","test","batman","trustno1","thomas","tigger","robert","access","love","buster","1234567","soccer","hockey","killer","george","sexy","andrew","charlie","superman","asshole","dallas","jessica","panties","pepper","1111","austin","william","daniel","golfer","summer","heather","hammer","yankees","joshua","maggie","biteme","enter","ashley","thunder","cowboy","silver","richard","orange","merlin","michelle","corvette","bigdog","cheese","matthew","121212","patrick","martin","freedom","ginger","blowjob","nicole","sparky","yellow","camaro","secret","dick","falcon","taylor","111111","131313","123123","bitch","hello","scooter","please","","porsche","guitar","chelsea","black","diamond","nascar","jackson","cameron","654321","computer","amanda","wizard","xxxxxxxx","money","phoenix","mickey","bailey","knight","iceman","tigers","purple","andrea","horny","dakota","aaaaaa","player","sunshine","morgan","starwars","boomer","cowboys","edward","charles","girls","booboo","coffee","xxxxxx","bulldog","ncc1701","rabbit","peanut","john","johnny","gandalf","spanky","winter","brandy","compaq","carlos","tennis","james","mike","brandon","fender","anthony","blowme","ferrari","cookie","chicken","maverick","chicago","joseph","diablo","sexsex","hardcore","666666","willie","welcome","chris","panther","yamaha","justin","banana","driver","marine","angels","fishing","david","maddog","hooters","wilson","butthead","dennis","captain","bigdick","chester","smokey","xavier","steven","viking","snoopy","blue","eagles","winner","samantha","house","miller","flower","jack","firebird","butter","united","turtle","steelers","tiffany","zxcvbn","tomcat","golf","bond007","bear","tiger","doctor","gateway","gators","angel","junior","thx1138","porno","badboy","debbie","spider","melissa","booger","1212","flyers","fish","porn","matrix","teens","scooby","jason","walter","cumshot","boston","braves","yankee","lover","barney","victor","tucker","princess","mercedes","5150","doggie","zzzzzz","gunner","horney","bubba","2112","fred","johnson","xxxxx","tits","member","boobs","donald","bigdaddy","bronco","penis","voyager","rangers","birdie","trouble","white","topgun","bigtits","bitches","green","super","qazwsx","magic","lakers","rachel","slayer","scott","2222","asdf","video","london","7777","marlboro","srinivas","internet","action","carter","jasper","monster","teresa","jeremy","11111111","bill","crystal","peter","pussies","cock","beer","rocket","theman","oliver","prince","beach","amateur","7777777","muffin","redsox","star","testing","shannon","murphy","frank","hannah","dave","eagle1","11111","mother","nathan","raiders","steve","forever","angela","viper","ou812","jake","lovers","suckit","gregory","buddy","whatever","young","nicholas","lucky","helpme","jackie","monica","midnight","college","baby","brian","mark","startrek","sierra","leather","232323","4444","beavis","bigcock","happy","sophie","ladies","naughty","giants","booty","blonde","golden","0","fire","sandra","pookie","packers","einstein","dolphins","0","chevy","winston","warrior","sammy","slut","8675309","zxcvbnm","nipples","power","victoria","asdfgh","vagina","toyota","travis","hotdog","paris","rock","xxxx","extreme","redskins","erotic","dirty","ford","freddy","arsenal","access14","wolf","nipple","iloveyou","alex","florida","eric","legend","movie","success","rosebud","jaguar","great","cool","cooper","1313","scorpio","mountain","madison","987654","brazil","lauren","japan","naked","squirt","stars","apple","alexis","aaaa","bonnie","peaches","jasmine","kevin","matt","qwertyui","danielle","beaver","4321","4128","runner","swimming","dolphin","gordon","casper","stupid","shit","saturn","gemini","apples","august","3333","canada","blazer","cumming","hunting","kitty","rainbow","112233","arthur","cream","calvin","shaved","surfer","samson","kelly","paul","mine","king","racing","5555","eagle","hentai","newyork","little","redwings","smith","sticky","cocacola","animal","broncos","private","skippy","marvin","blondes","enjoy","girl","apollo","parker","qwert","time","sydney","women","voodoo","magnum","juice","abgrtyu","777777","dreams","maxwell","music","rush2112","russia","scorpion","rebecca","tester","mistress","phantom","billy","6666","albert"];
+    let ForbiddenSequences = ["123456","password","12345678","1234","pussy","12345","aaAA11!!","dragon","qwerty","696969","mustang","letmein","baseball","master","michael","football","shadow","monkey","abc123","pass","6969","jordan","harley","ranger","iwantu","jennifer","hunter","2000","test","batman","trustno1","thomas","tigger","robert","access","love","buster","1234567","soccer","hockey","killer","george","sexy","andrew","charlie","superman","asshole","dallas","jessica","panties","pepper","1111","austin","william","daniel","golfer","summer","heather","hammer","yankees","joshua","maggie","biteme","enter","ashley","thunder","cowboy","silver","richard","orange","merlin","michelle","corvette","bigdog","cheese","matthew","121212","patrick","martin","freedom","ginger","blowjob","nicole","sparky","yellow","camaro","secret","dick","falcon","taylor","111111","131313","123123","bitch","hello","scooter","please","","porsche","guitar","chelsea","black","diamond","nascar","jackson","cameron","654321","computer","amanda","wizard","xxxxxxxx","money","phoenix","mickey","bailey","knight","iceman","tigers","purple","andrea","horny","dakota","aaaaaa","player","sunshine","morgan","starwars","boomer","cowboys","edward","charles","girls","booboo","coffee","xxxxxx","bulldog","ncc1701","rabbit","peanut","john","johnny","gandalf","spanky","winter","brandy","compaq","carlos","tennis","james","mike","brandon","fender","anthony","blowme","ferrari","cookie","chicken","maverick","chicago","joseph","diablo","sexsex","hardcore","666666","willie","welcome","chris","panther","yamaha","justin","banana","driver","marine","angels","fishing","david","maddog","hooters","wilson","butthead","dennis","captain","bigdick","chester","smokey","xavier","steven","viking","snoopy","blue","eagles","winner","samantha","house","miller","flower","jack","firebird","butter","united","turtle","steelers","tiffany","zxcvbn","tomcat","golf","bond007","bear","tiger","doctor","gateway","gators","angel","junior","thx1138","porno","badboy","debbie","spider","melissa","booger","1212","flyers","fish","porn","matrix","teens","scooby","jason","walter","cumshot","boston","braves","yankee","lover","barney","victor","tucker","princess","mercedes","5150","doggie","zzzzzz","gunner","horney","bubba","2112","fred","johnson","xxxxx","tits","member","boobs","donald","bigdaddy","bronco","penis","voyager","rangers","birdie","trouble","white","topgun","bigtits","bitches","green","super","qazwsx","magic","lakers","rachel","slayer","scott","2222","asdf","video","london","7777","marlboro","srinivas","internet","action","carter","jasper","monster","teresa","jeremy","11111111","bill","crystal","peter","pussies","cock","beer","rocket","theman","oliver","prince","beach","amateur","7777777","muffin","redsox","star","testing","shannon","murphy","frank","hannah","dave","eagle1","11111","mother","nathan","raiders","steve","forever","angela","viper","ou812","jake","lovers","suckit","gregory","buddy","whatever","young","nicholas","lucky","helpme","jackie","monica","midnight","college","baby","brian","mark","startrek","sierra","leather","232323","4444","beavis","bigcock","happy","sophie","ladies","naughty","giants","booty","blonde","golden","0","fire","sandra","pookie","packers","einstein","dolphins","0","chevy","winston","warrior","sammy","slut","8675309","zxcvbnm","nipples","power","victoria","asdfgh","vagina","toyota","travis","hotdog","paris","rock","xxxx","extreme","redskins","erotic","dirty","ford","freddy","arsenal","access14","wolf","nipple","iloveyou","alex","florida","eric","legend","movie","success","rosebud","jaguar","great","cool","cooper","1313","scorpio","mountain","madison","987654","brazil","lauren","japan","naked","squirt","stars","apple","alexis","aaaa","bonnie","peaches","jasmine","kevin","matt","qwertyui","danielle","beaver","4321","4128","runner","swimming","dolphin","gordon","casper","stupid","shit","saturn","gemini","apples","august","3333","canada","blazer","cumming","hunting","kitty","rainbow","112233","arthur","cream","calvin","shaved","surfer","samson","kelly","paul","mine","king","racing","5555","eagle","hentai","newyork","little","redwings","smith","sticky","cocacola","animal","broncos","private","skippy","marvin","blondes","enjoy","girl","apollo","parker","qwert","time","sydney","women","voodoo","magnum","juice","abgrtyu","777777","dreams","maxwell","music","rush2112","russia","scorpion","rebecca","tester","mistress","phantom","billy","6666","albert"];
     if (ForbiddenSequences.includes(passwordField.value.toLowerCase().trim())) {
       return true;
     }
@@ -221,6 +252,56 @@ const formChecker = {
 
     // Tooltip
     if(type == 'tooltip'){
+      let tooltipWrapper = document.createElement('div'),
+      tooltipContainer = document.createElement('div'),
+      tooltipTitle = document.createElement('h3'),
+      passwordFields = element.querySelectorAll('input[type="password"]'),
+      mustContain = document.createElement('div'),
+      firstPasswordField = passwordFields[0],
+      lastPasswordField = passwordFields[passwordFields.length - 1];
+
+      tooltipWrapper.classList.add('tooltip-wrapper');
+      tooltipContainer.classList.add('tooltip-container');
+      tooltipTitle.classList.add('tooltip-title');
+      mustContain.classList.add('tooltip-inner')
+      tooltipTitle.innerHTML = `${formChecker.tooltipTitle}`;
+
+
+      if (formChecker.specialCharacters > 0) {
+        mustContain.innerHTML = `${mustContain.innerHTML} <span class="specialCharacters">${this.specialCharacters} ${(this.tooltipNames['specialCharacters'] || 'special characters')}</span>`;
+      }
+  
+      if (formChecker.numbers > 0) {
+        mustContain.innerHTML = `${mustContain.innerHTML} <span class="numbers">${this.numbers} ${(this.tooltipNames['numbers'] || 'numbers')}</span>`;
+      }
+      
+      if (formChecker.capitalLetters > 0) {
+        mustContain.innerHTML = `${mustContain.innerHTML} <span class="capitalLetters">${this.capitalLetters} ${(this.tooltipNames['capitalLetters'] || 'capital letters')}</span>`;
+      }
+  
+      if (formChecker.lowerCaseLetters > 0) {
+        mustContain.innerHTML = `${mustContain.innerHTML} <span class="lowerCaseLetters">${this.lowerCaseLetters} ${(this.tooltipNames['lowerCaseLetters'] || 'lowercase letters')}</span>`;
+      }
+
+      tooltipContainer.appendChild(tooltipTitle);
+      tooltipContainer.appendChild(mustContain);
+      tooltipWrapper.appendChild(tooltipContainer);
+      
+      lastPasswordField.parentNode.insertBefore(tooltipWrapper, lastPasswordField.nextSibling);
+
+      // Toggle 
+      firstPasswordField.addEventListener('focusin', function (){
+        tooltipWrapper.classList.add('active');
+      });
+
+      firstPasswordField.addEventListener('focusout', function (){
+        tooltipWrapper.classList.remove('active');
+      });
+
+      firstPasswordField.addEventListener('keyup', function () {
+        formChecker.passwordChecker(element,'tooltip');
+      });
+
       
       return;
     }
@@ -236,7 +317,9 @@ const formChecker = {
 
   passwordChecker: function (element,type) {
 
-    let firstPasswordField = element.querySelectorAll('input[type="password"]')[0],
+    let passwordFields = element.querySelectorAll('input[type="password"]'),
+    firstPasswordField = passwordFields[0],
+    lastPasswordField = passwordFields[passwordFields.length - 1],
     filledInPassword = firstPasswordField.value,
     filled = [];
 
@@ -272,17 +355,35 @@ const formChecker = {
     if((filledInPassword.match(/[!@#$%^&]/g) || []).length >= this.specialCharacters) {
       filled['specialCharacters'] = true;
     }
+
+
     let filledCounter = 0,
     maxFilled = 0;
 
     for (const key in filled) {
       if (Object.hasOwnProperty.call(filled, key)) {
         maxFilled++;
+        if(type == 'tooltip'){ element.querySelectorAll('.tooltip-inner')[0].classList.remove(key); }
+
         if(filled[key] == true){
           filledCounter++;
+          if(type == 'tooltip'){
+            element.querySelectorAll('.tooltip-inner')[0].classList.add(key);
+          }
+
         }
       }
     }
+
+     // Enable repeat 
+     if(passwordFields.length > 1){ 
+      lastPasswordField.setAttribute('disabled',true);
+      if (maxFilled <= filledCounter) {
+        lastPasswordField.removeAttribute('disabled');
+      }
+    }
+
+    if(type == 'tooltip'){ return; }
 
     let responseElement = element.querySelectorAll('.progress-bar-container')[0];
 
@@ -291,21 +392,21 @@ const formChecker = {
     }
 
     if(filledInPassword.length <= this.miniumPasswordLength || (element.hasAttribute('data-common-password') && this.commonPassword(element))){
-      formChecker.respone(responseElement,'weak');
+      formChecker.response(responseElement,'weak');
       return;
     }
 
     if (maxFilled <= filledCounter) {
-      formChecker.respone(responseElement,'strong');
+      formChecker.response(responseElement,'strong');
       return;
     }
 
     if ((maxFilled / 2) <= filledCounter) {
-      formChecker.respone(responseElement,'medium');
+      formChecker.response(responseElement,'medium');
       return;
     }
     
-    formChecker.respone(responseElement,'weak');
+    formChecker.response(responseElement,'weak');
     
   },
 
